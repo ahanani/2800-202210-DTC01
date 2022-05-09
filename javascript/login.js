a91876005e4870fb5bd316907f39f672dcdf5f5e
ahanai <ahanani@my.bcit.ca>
2022-05-09 11:17:40 -0700

    Added login page

---
+++ javascript/login.js
@@ -0,0 +1,18 @@
+window.onload = () => {
+    document.getElementById("userId").value = "";
+}
+
+
+document.getElementById("submit").addEventListener("click", function() {
+    let userId = (document.getElementById("userId").value);
+    let userPassword = document.getElementById("userPassword").value;
+
+    let xhttp =  new XMLHttpRequest();
+
+    xhttp.onload = () => {
+
+    }
+
+    xhttp.open("GET", "/users?id=" + userId + "&password=" + userPassword, true);
+    xhttp.send();
+});
