26b9c59abf2db26ac18bb45d3dfd285b94424e1c
Amin Jafarpour <a.jafarpour2001@gmail.com>
2022-05-15 21:47:28 -0700

    ...

--- session/session-db.js
+++ session/session-db.js
@@ -1,5 +1,9 @@
 function parseResultSet(resultset) {
 
+    if (resultset == undefined) {
+        return undefined;
+    }
+
     parsedResult = [];
 
     for (let i = 0; i < resultset[1].length; ++i) {
@@ -13,6 +17,19 @@ function parseResultSet(resultset) {
 function duplicateUserName(newUserName, parsedResultSet) {
     for (let i = 0; i < parsedResultSet.length; ++i) {
         if (parsedResultSet[i][0] == newUserName) {
+            return true;
+        }
+    }
+
+    return false;
+}
+
+function duplicateCompany(newCompanyName, parsedResultSet) {
+    if (parsedResultSet == undefined) {
+        return false;
+    }
+    for (let i = 0; i < parsedResultSet.length; ++i) {
+        if (parsedResultSet[i][0] == newCompanyName) {
             return true;
         }
     }
@@ -83,8 +100,6 @@ function insertUser(userData) {
         }
     });
 
-
-
     const queryStatement = `USE dtc01; INSERT INTO user VALUES("${userData.username}", 
     "${userData.firstname}", "${userData.lastname}", "${userData.password}");`;
 
@@ -129,10 +144,36 @@ function insertCsvItem(username, csvItem) {
     });
 }
 
+function insertCompany(companyName, companyDetails) {
+    connect();
+
+    connection.query(`USE dtc01; SELECT Companyname FROM company;`, function(err, result) {
+        if (err) console.log(err);
+        let duplicate = duplicateCompany(companyName, parseResultSet(result));
+        if (duplicate) {
+            throw "Duplicate comapny name";
+        }
+    });
+
+    const queryStatement = `USE dtc01; INSERT INTO Company(Companyname, Companyindustry, Companydescription) VALUES(${formatStringItem(companyName)}, 
+        ${formatStringItem(companyDetails.companyindustry)}, 
+        ${formatStringItem(companyDetails.companydescription)});`;
+
+    connection.query(queryStatement, function(err, result) {
+        if (err) console.log(err);
+        console.log(result);
+
+        //closeConnection();
+    });
+}
+
+insertCompany("burger king", { companyindustry: "fast food", companydescription: "junk fast food company" });
+
 
 module.exports = {
     insertUser,
     validateUser,
-    insertCsvItem
+    insertCsvItem,
+    insertCompany
 
 }
